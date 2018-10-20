e79760ca3fe36630021b434a45217974ff48c6ec
Jesse Hoobergs <jesse@hoobergs.be>
2018-10-20 15:02:57 +0200

    update render script

--- scripts/render.sh
+++ scripts/render.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-PBRT_PATH="../build/"
-SCENE_PATH="../scenes/"
-RESULTS_PATH="../results/"
-echo $1
-$PBRT_PATH"pbrt" --outfile $RESULTS_PATH$1".png" $SCENE_PATH$1".pbrt"
+BASE="/Programming/Thesis/pbrt-v3"
+PBRT_PATH=$BASE"/build/"
+SCENE_PATH=$BASE/"scenes/"$1".pbrt"
+DATE=`date '+%Y-%m-%d-%H:%M:%S'`
+RESULTS_DIR=$BASE"/results/"$1"/"$DATE"/"
+FILE_NAME='output.png'
+
+mkdir -p $RESULTS_DIR
+cp $SCENE_PATH $RESULTS_DIR"input.pbrt"
+$PBRT_PATH"pbrt" --outfile $RESULTS_DIR$FILE_NAME $SCENE_PATH
