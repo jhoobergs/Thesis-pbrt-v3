c6631df8088e6db3e1e89c0ddd68d0800901b667
Jesse Hoobergs <jesse@hoobergs.be>
2018-10-20 15:39:32 +0200

    Update render script to keep render output & stats

--- scripts/render.sh
+++ scripts/render.sh
@@ -8,4 +8,6 @@ FILE_NAME='output.png'
 
 mkdir -p $RESULTS_DIR
 cp $SCENE_PATH $RESULTS_DIR"input.pbrt"
-$PBRT_PATH"pbrt" --outfile $RESULTS_DIR$FILE_NAME $SCENE_PATH
+$PBRT_PATH"pbrt" --outfile $RESULTS_DIR$FILE_NAME $SCENE_PATH 2>&1 | tee $RESULTS_DIR/"renderOutput"
+tail --lines=+$(cat $RESULTS_DIR/"renderOutput" | grep -n "Statistics" | cut -f1 -d:) $RESULTS_DIR/"renderOutput" > $RESULTS_DIR/"statistics"
+xdg-open $RESULTS_DIR$FILE_NAME
