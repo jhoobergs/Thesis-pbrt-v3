fb745c8e026356785fd850c36af1478a2c571785
Jesse Hoobergs <jesse@hoobergs.be>
2018-11-03 18:29:59 +0100

    reformat code

--- src/accelerators/kdtreeaccel.h
+++ src/accelerators/kdtreeaccel.h
@@ -45,47 +45,55 @@
 namespace pbrt {
 
 // KdTreeAccel Declarations
-struct KdAccelNode;
-struct BoundEdge;
-class KdTreeAccel : public Aggregate {
-  public:
-    // KdTreeAccel Public Types
-    enum class AxisHeuristic { Longest, All };
+    struct KdAccelNode;
+    struct BoundEdge;
 
-    // KdTreeAccel Public Methods
-    KdTreeAccel(std::vector<std::shared_ptr<Primitive>> p,
-                int isectCost = 80, int traversalCost = 1,
-                Float emptyBonus = 0.5, int maxPrims = 1, int maxDepth = -1, AxisHeuristic axisHeuristic = AxisHeuristic::Longest);
-    Bounds3f WorldBound() const { return bounds; }
-    ~KdTreeAccel();
-    bool Intersect(const Ray &ray, SurfaceInteraction *isect) const;
-    bool IntersectP(const Ray &ray) const;
+    class KdTreeAccel : public Aggregate {
+    public:
+        // KdTreeAccel Public Types
+        enum class AxisHeuristic {
+            Longest, All
+        };
 
-  private:
-    // KdTreeAccel Private Methods
-    void buildTree(AxisHeuristic axisHeuristic, int nodeNum, const Bounds3f &bounds,
-                   const std::vector<Bounds3f> &primBounds, int *primNums,
-                   int nprims, int depth,
-                   const std::unique_ptr<BoundEdge[]> edges[3], int *prims0,
-                   int *prims1, int badRefines = 0);
+        // KdTreeAccel Public Methods
+        KdTreeAccel(std::vector<std::shared_ptr<Primitive>> p,
+                    int isectCost = 80, int traversalCost = 1,
+                    Float emptyBonus = 0.5, int maxPrims = 1, int maxDepth = -1,
+                    AxisHeuristic axisHeuristic = AxisHeuristic::Longest);
 
-    // KdTreeAccel Private Data
-    const int isectCost, traversalCost, maxPrims;
-    const Float emptyBonus;
-    std::vector<std::shared_ptr<Primitive>> primitives;
-    std::vector<int> primitiveIndices;
-    KdAccelNode *nodes;
-    int nAllocedNodes, nextFreeNode;
-    Bounds3f bounds;
-};
+        Bounds3f WorldBound() const { return bounds; }
 
-struct KdToDo {
-    const KdAccelNode *node;
-    Float tMin, tMax;
-};
+        ~KdTreeAccel();
 
-std::shared_ptr<KdTreeAccel> CreateKdTreeAccelerator(
-    std::vector<std::shared_ptr<Primitive>> prims, const ParamSet &ps);
+        bool Intersect(const Ray &ray, SurfaceInteraction *isect) const;
+
+        bool IntersectP(const Ray &ray) const;
+
+    private:
+        // KdTreeAccel Private Methods
+        void buildTree(AxisHeuristic axisHeuristic, int nodeNum, const Bounds3f &bounds,
+                       const std::vector<Bounds3f> &primBounds, int *primNums,
+                       int nprims, int depth,
+                       const std::unique_ptr<BoundEdge[]> edges[3], int *prims0,
+                       int *prims1, int badRefines = 0);
+
+        // KdTreeAccel Private Data
+        const int isectCost, traversalCost, maxPrims;
+        const Float emptyBonus;
+        std::vector<std::shared_ptr<Primitive>> primitives;
+        std::vector<int> primitiveIndices;
+        KdAccelNode *nodes;
+        int nAllocedNodes, nextFreeNode;
+        Bounds3f bounds;
+    };
+
+    struct KdToDo {
+        const KdAccelNode *node;
+        Float tMin, tMax;
+    };
+
+    std::shared_ptr<KdTreeAccel> CreateKdTreeAccelerator(
+            std::vector<std::shared_ptr<Primitive>> prims, const ParamSet &ps);
 
 }  // namespace pbrt
 
