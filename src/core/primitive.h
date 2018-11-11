f7da0702a556314f08c1625b9ad5bc10e12c4c21
Jesse Hoobergs <jesse@hoobergs.be>
2018-11-11 23:13:45 +0100

    Add boundsf and BoundsMf

--- src/core/primitive.h
+++ src/core/primitive.h
@@ -48,83 +48,107 @@
 namespace pbrt {
 
 // Primitive Declarations
-class Primitive {
-  public:
-    // Primitive Interface
-    virtual ~Primitive();
-    virtual Bounds3f WorldBound() const = 0;
-    virtual bool Intersect(const Ray &r, SurfaceInteraction *) const = 0;
-    virtual bool IntersectP(const Ray &r) const = 0;
-    virtual const AreaLight *GetAreaLight() const = 0;
-    virtual const Material *GetMaterial() const = 0;
-    virtual void ComputeScatteringFunctions(SurfaceInteraction *isect,
-                                            MemoryArena &arena,
-                                            TransportMode mode,
-                                            bool allowMultipleLobes) const = 0;
-};
+    class Primitive {
+    public:
+        // Primitive Interface
+        virtual ~Primitive();
+
+        virtual Bounds3f WorldBound() const = 0;
+
+        virtual bool Intersect(const Ray &r, SurfaceInteraction *) const = 0;
+
+        virtual bool IntersectP(const Ray &r) const = 0;
+
+        virtual const AreaLight *GetAreaLight() const = 0;
+
+        virtual const Material *GetMaterial() const = 0;
+
+        virtual void ComputeScatteringFunctions(SurfaceInteraction *isect,
+                                                MemoryArena &arena,
+                                                TransportMode mode,
+                                                bool allowMultipleLobes) const = 0;
+
+        virtual Boundsf getBounds(Vector3f direction) const = 0;
+    };
 
 // GeometricPrimitive Declarations
-class GeometricPrimitive : public Primitive {
-  public:
-    // GeometricPrimitive Public Methods
-    virtual Bounds3f WorldBound() const;
-    virtual bool Intersect(const Ray &r, SurfaceInteraction *isect) const;
-    virtual bool IntersectP(const Ray &r) const;
-    GeometricPrimitive(const std::shared_ptr<Shape> &shape,
-                       const std::shared_ptr<Material> &material,
-                       const std::shared_ptr<AreaLight> &areaLight,
-                       const MediumInterface &mediumInterface);
-    const AreaLight *GetAreaLight() const;
-    const Material *GetMaterial() const;
-    void ComputeScatteringFunctions(SurfaceInteraction *isect,
-                                    MemoryArena &arena, TransportMode mode,
-                                    bool allowMultipleLobes) const;
+    class GeometricPrimitive : public Primitive {
+    public:
+        // GeometricPrimitive Public Methods
+        virtual Bounds3f WorldBound() const;
 
-  private:
-    // GeometricPrimitive Private Data
-    std::shared_ptr<Shape> shape;
-    std::shared_ptr<Material> material;
-    std::shared_ptr<AreaLight> areaLight;
-    MediumInterface mediumInterface;
-};
+        virtual bool Intersect(const Ray &r, SurfaceInteraction *isect) const;
+
+        virtual bool IntersectP(const Ray &r) const;
+
+        GeometricPrimitive(const std::shared_ptr<Shape> &shape,
+                           const std::shared_ptr<Material> &material,
+                           const std::shared_ptr<AreaLight> &areaLight,
+                           const MediumInterface &mediumInterface);
+
+        const AreaLight *GetAreaLight() const;
+
+        const Material *GetMaterial() const;
+
+        void ComputeScatteringFunctions(SurfaceInteraction *isect,
+                                        MemoryArena &arena, TransportMode mode,
+                                        bool allowMultipleLobes) const;
+
+        Boundsf getBounds(Vector3f direction) const;
+
+    private:
+        // GeometricPrimitive Private Data
+        std::shared_ptr<Shape> shape;
+        std::shared_ptr<Material> material;
+        std::shared_ptr<AreaLight> areaLight;
+        MediumInterface mediumInterface;
+    };
 
 // TransformedPrimitive Declarations
-class TransformedPrimitive : public Primitive {
-  public:
-    // TransformedPrimitive Public Methods
-    TransformedPrimitive(std::shared_ptr<Primitive> &primitive,
-                         const AnimatedTransform &PrimitiveToWorld);
-    bool Intersect(const Ray &r, SurfaceInteraction *in) const;
-    bool IntersectP(const Ray &r) const;
-    const AreaLight *GetAreaLight() const { return nullptr; }
-    const Material *GetMaterial() const { return nullptr; }
-    void ComputeScatteringFunctions(SurfaceInteraction *isect,
-                                    MemoryArena &arena, TransportMode mode,
-                                    bool allowMultipleLobes) const {
-        LOG(FATAL) <<
-            "TransformedPrimitive::ComputeScatteringFunctions() shouldn't be "
-            "called";
-    }
-    Bounds3f WorldBound() const {
-        return PrimitiveToWorld.MotionBounds(primitive->WorldBound());
-    }
+    class TransformedPrimitive : public Primitive {
+    public:
+        // TransformedPrimitive Public Methods
+        TransformedPrimitive(std::shared_ptr<Primitive> &primitive,
+                             const AnimatedTransform &PrimitiveToWorld);
 
-  private:
-    // TransformedPrimitive Private Data
-    std::shared_ptr<Primitive> primitive;
-    const AnimatedTransform PrimitiveToWorld;
-};
+        bool Intersect(const Ray &r, SurfaceInteraction *in) const;
+
+        bool IntersectP(const Ray &r) const;
+
+        const AreaLight *GetAreaLight() const { return nullptr; }
+
+        const Material *GetMaterial() const { return nullptr; }
+
+        void ComputeScatteringFunctions(SurfaceInteraction *isect,
+                                        MemoryArena &arena, TransportMode mode,
+                                        bool allowMultipleLobes) const {
+            LOG(FATAL) <<
+                       "TransformedPrimitive::ComputeScatteringFunctions() shouldn't be "
+                       "called";
+        }
+
+        Bounds3f WorldBound() const {
+            return PrimitiveToWorld.MotionBounds(primitive->WorldBound());
+        }
+
+    private:
+        // TransformedPrimitive Private Data
+        std::shared_ptr<Primitive> primitive;
+        const AnimatedTransform PrimitiveToWorld;
+    };
 
 // Aggregate Declarations
-class Aggregate : public Primitive {
-  public:
-    // Aggregate Public Methods
-    const AreaLight *GetAreaLight() const;
-    const Material *GetMaterial() const;
-    void ComputeScatteringFunctions(SurfaceInteraction *isect,
-                                    MemoryArena &arena, TransportMode mode,
-                                    bool allowMultipleLobes) const;
-};
+    class Aggregate : public Primitive {
+    public:
+        // Aggregate Public Methods
+        const AreaLight *GetAreaLight() const;
+
+        const Material *GetMaterial() const;
+
+        void ComputeScatteringFunctions(SurfaceInteraction *isect,
+                                        MemoryArena &arena, TransportMode mode,
+                                        bool allowMultipleLobes) const;
+    };
 
 }  // namespace pbrt
 
