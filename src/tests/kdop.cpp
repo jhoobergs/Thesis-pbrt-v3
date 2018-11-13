1485d40e18277b1a3fbd6df8387c2a38825011c7
Jesse Hoobergs <jesse@hoobergs.be>
2018-11-13 12:16:42 +0100

    Extra kdop cut test

--- src/tests/kdop.cpp
+++ src/tests/kdop.cpp
@@ -213,7 +213,60 @@ TEST(kDOP, CutDiag) {
 
     }
     EXPECT_EQ(9, result.first.edges.size());
+    EXPECT_EQ(9, result.second.edges.size());
+
 
     EXPECT_FLOAT_EQ(8.5 * 22 + 22 * 1 + 8.5 * 1 + 1 * (float) std::sqrt(22 * 22 + 8.5 * 8.5), result.first.SurfaceArea(directions));
     EXPECT_FLOAT_EQ(8.5 * 22 + 22 * 1 + 8.5 * 1 + 1 * (float) std::sqrt(22 * 22 + 8.5 * 8.5), result.second.SurfaceArea(directions));
+}
+
+
+TEST(kDOP, CutArb) {
+    std::vector<Vector3f> directions;
+    directions.emplace_back(Vector3f(1.0, 0.0, 0.0));
+    directions.emplace_back(Vector3f(0.0, 1.0, 0.0));
+    directions.emplace_back(Vector3f(0.0, 0.0, 1.0));
+    directions.emplace_back(Vector3f(1 / (float) std::sqrt(2), 1 / (float) std::sqrt(2),0));
+
+    Bounds3f bounds = Bounds3f(Point3f(-5, 2, 0), Point3f(3.5, 3, 22));
+
+    KDOPMesh kDOPMesh;
+    Point3f v1 = Point3f(bounds.pMin);
+    Point3f v2 = Point3f(bounds.pMin.x, bounds.pMin.y, bounds.pMax.z);
+    Point3f v3 = Point3f(bounds.pMin.x, bounds.pMax.y, bounds.pMin.z);
+    Point3f v4 = Point3f(bounds.pMax.x, bounds.pMin.y, bounds.pMin.z);
+    Point3f v5 = Point3f(bounds.pMin.x, bounds.pMax.y, bounds.pMax.z);
+    Point3f v6 = Point3f(bounds.pMax.x, bounds.pMin.y, bounds.pMax.z);
+    Point3f v7 = Point3f(bounds.pMax.x, bounds.pMax.y, bounds.pMin.z);
+    Point3f v8 = Point3f(bounds.pMax);
+
+    kDOPMesh.addEdge(KDOPEdge(&v1, &v2, 1, 3));
+    kDOPMesh.addEdge(KDOPEdge(&v1, &v3, 1, 5));
+    kDOPMesh.addEdge(KDOPEdge(&v1, &v4, 3, 5));
+    kDOPMesh.addEdge(KDOPEdge(&v2, &v5, 1, 4));
+    kDOPMesh.addEdge(KDOPEdge(&v2, &v6, 3, 4));
+    kDOPMesh.addEdge(KDOPEdge(&v3, &v5, 1, 2));
+    kDOPMesh.addEdge(KDOPEdge(&v3, &v7, 2, 5));
+    kDOPMesh.addEdge(KDOPEdge(&v4, &v6, 0, 3));
+    kDOPMesh.addEdge(KDOPEdge(&v4, &v7, 0, 5));
+    kDOPMesh.addEdge(KDOPEdge(&v5, &v8, 2, 4));
+    kDOPMesh.addEdge(KDOPEdge(&v6, &v8, 0, 4));
+    kDOPMesh.addEdge(KDOPEdge(&v7, &v8, 0, 2));
+
+    std::pair<KDOPMesh, KDOPMesh> result = kDOPMesh.cut(directions.size(), 2.8 * (float) std::sqrt(2),
+                                                        directions[3], 3);
+    for (auto &edge: result.first.edges) {
+        Warning("FIRST: %d and %d: (%f,%f,%f) to (%f,%f,%f)", edge.faceId1, edge.faceId2, edge.v1->x, edge.v1->y, edge.v1->z,
+                edge.v2->x, edge.v2->y, edge.v2->z);
+    }
+
+    for (auto &edge: result.second.edges) {
+        Warning("SECOND: %d and %d: (%f,%f,%f) to (%f,%f,%f)", edge.faceId1, edge.faceId2, edge.v1->x, edge.v1->y, edge.v1->z,
+                edge.v2->x, edge.v2->y, edge.v2->z);
+    }
+    EXPECT_EQ(15, result.first.edges.size());
+    EXPECT_EQ(9, result.second.edges.size());
+
+    EXPECT_FLOAT_EQ(2*7.6 * (22+1) + 22 + 0.9 * (22 + 2*0.1) + 0.1*22 + 0.9*0.9 + 0.9*std::sqrt(2)*22, result.first.SurfaceArea(directions));
+    EXPECT_FLOAT_EQ(2* 0.9*22 + 0.9*0.9 + 0.9*std::sqrt(2)*22, result.second.SurfaceArea(directions));
 }
